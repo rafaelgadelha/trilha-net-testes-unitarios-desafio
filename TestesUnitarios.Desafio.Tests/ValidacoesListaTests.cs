c5c3d798568841e5ca6b41785a075aabe84b0ed8
Rafael Gadelha <gadellharafaell@gmail.com>
2024-01-05 09:57:32 -0300

    DeveMultiplicarOsElementosDaListaPor2 - corrigido!

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -52,6 +52,7 @@ public class ValidacoesListaTests
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
@@ -61,8 +62,11 @@ public class ValidacoesListaTests
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
         
         // Act
-
+        var resultado = _validacoes.MultiplicarNumerosLista(lista, 2);
+        
         // Assert
+        Assert.Equal(resultadoEsperado, resultado);
+
     }
 
     [Fact]
